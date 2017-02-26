f208262683848104e68d220af7aeb377f040de3d
Alex Rossi Ignez <Alex Rossi Ignez>
2017-02-26 00:24:54 -0300

    half of getChildren (rooms) is done, tests passing, need more tests, check getChildren.tests.js comments

--- src/functions/getChildren.js
+++ src/functions/getChildren.js
@@ -2,40 +2,78 @@ const getChildren = (
 	  spaceAround, room, {minW, maxW, minH, maxH} 
 ) => {
 	const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min +1)) + min;
+
+	//left
+		let left = {};
+		left.endCol = spaceAround.left.endCol- 1;
+
+		left.minStartCol = left.endCol - maxW;
+		left.maxStartCol = left.endCol - minW;
+		left.startCol = getRandomInt(left.minStartCol, left.maxStartCol);	
+
+		left.minStartRow = (spaceAround.left.startRow) > (room.startRow +1 - maxH) ? (spaceAround.left.startRow) : (room.startRow +1 -max); 
+		left.maxStartRow = (room.endRow -1) < (spaceAround.left.endRow - minH) ? (room.endRow -1) : (spaceAround.left.endRow - minH);
+		left.startRow = getRandomInt(left.minStartRow, left.maxStartRow);
+
+		left.minEndRow = (left.startRow + minH) > (room.startRow +1) ? (left.startRow + minH) : (room.startRow +1) ; 
+		left.maxEndRow = spaceAround.left.endRow < (left.startRow + maxH) ? spaceAround.left.endRow : (left.startRow + maxH); 
+		left.endRow = getRandomInt(left.minEndRow, left.maxEndRow);
 	
-	//Left coords
-	let left = {}
-		// horizontal
-			left.endCol = room.startCol -1;
-			left.startCol = getRandomInt(left.endCol - maxW, left.endCol - minW)
-		//vertical
-			left.startRow = getRandomInt(spaceAround.left.startRow, room.endRow-1); 
-			let minEndRow = (left.startRow + minH) > room.startRow 
-											? (left.startRow + minH)
-											: (room.startRow + 1);
-			let maxEndRow = (left.startRow + maxH) < spaceAround.left.endRow 
-											? (left.startRow + maxH)
-											: spaceAround.left.endRow;
-			left.endRow = getRandomInt(minEndRow, maxEndRow);
-	//Right coords
-	let right = {}
-		// horizontal
-			right.startCol = room.endCol +1;
-			right.endCol = getRandomInt(right.startCol + minW, right.startCol + maxW)
-		//vertical
-			right.startRow = getRandomInt(spaceAround.right.startRow, room.endRow-1); 
-			minEndRow = (right.startRow + minH) > room.startRow 
-											? (right.startRow + minH)
-											: (room.startRow + 1);
-			maxEndRow = (right.startRow + maxH) < spaceAround.right.endRow 
-											? (right.startRow + maxH)
-											: spaceAround.right.endRow;
-			right.endRow = getRandomInt(minEndRow, maxEndRow);
+	//right
+		let right = {};
+		right.startCol = spaceAround.right.startCol+1;
+		
+		right.minEndCol = right.startCol + minW;
+		right.maxEndCol = right.startCol + maxW;
+		right.endCol = getRandomInt(right.minEndCol, right.maxEndCol)
+
+		right.minStartRow = (spaceAround.right.startRow) > (room.startRow +1 - maxH) ? (spaceAround.right.startRow) : (room.startRow +1 -max); 
+		right.maxStartRow = (room.endRow -1) < (spaceAround.right.endRow - minH) ? (room.endRow -1) : (spaceAround.right.endRow - minH);
+		right.startRow = getRandomInt(right.minStartRow, right.maxStartRow);
+
+		right.minEndRow = (right.startRow + minH) > (room.startRow +1) ? (right.startRow + minH) : (room.startRow +1) ; 
+		right.maxEndRow = spaceAround.right.endRow < (right.startRow + maxH) ? spaceAround.right.endRow : (right.startRow + maxH); 
+		right.endRow = getRandomInt(right.minEndRow, right.maxEndRow);
+
+	
+	//top
+		let top = {};
+		top.endRow = spaceAround.top.endRow-1;
+
+		top.minStartRow = (top.endRow - maxH) > spaceAround.top.startRow ? (top.endRow - maxH) : spaceAround.top.startRow; 
+		top.maxStartRow = top.endRow - minH;
+		top.startRow = getRandomInt(top.minStartRow, top.maxStartRow);
+	
+		top.minStartCol = (spaceAround.top.startCol) > (room.startCol +1 - minW) ? (spaceAround.top.startCol) : (room.startCol +1 -minW);
+		top.maxStartCol = (room.endCol -1) < (spaceAround.top.endCol - minW) ? (room.endCol -1) : (spaceAround.top.endCol - minW); 
+		top.startCol = getRandomInt(top.minStartCol, top.maxStartCol)
+
+		top.minEndCol =  top.startCol + minW; 
+		top.maxEndCol =  spaceAround.top.endCol < (top.startCol + maxW) ? spaceAround.top.endCol : (top.startCol + maxW);
+		top.endCol = getRandomInt(top.minEndCol, top.maxEndCol)
+
+	//bottom
+		let bottom = {};
+		bottom.startRow = spaceAround.bottom.startRow+1;
+
+		bottom.minEndRow = bottom.startRow + minH;
+		bottom.maxEndRow = bottom.startRow + maxH;
+		bottom.endRow = getRandomInt(bottom.minEndRow, bottom.maxEndRow)
+
+		bottom.minStartCol = (spaceAround.bottom.startCol) > (room.startCol +1 - minW) ? (spaceAround.bottom.startCol) : (room.startCol +1 -minW);
+		bottom.maxStartCol = (room.endCol -1) < (spaceAround.bottom.endCol - minW) ? (room.endCol -1) : (spaceAround.bottom.endCol - minW); 
+		bottom.startCol = getRandomInt(bottom.minStartCol, bottom.maxStartCol)
+
+		bottom.minEndCol =  bottom.startCol + minW; 
+		bottom.maxEndCol =  spaceAround.bottom.endCol  < (bottom.startCol + maxW) ? spaceAround.bottom.endCol : (bottom.startCol + maxW);
+		bottom.endCol = getRandomInt(bottom.minEndCol, bottom.maxEndCol)
 
 
 	return {
 		left,
-		right
+		right,
+		top,
+		bottom
 	}
 }	
 export default getChildren;
--- src/functions/getChildren.test.js
+++ src/functions/getChildren.test.js
@@ -15,85 +15,134 @@ describe("Given return of getSpaceAround, coordsOfMother, min width and min heig
 			startCol: 1,
 			endRow: 20,
 			endCol: 25	
-		},
+		},//18 rows 24 cols
 		right: {
 			startRow: 5,
 			startCol: 37,
 			endRow: 40,
-			endCol: 47	
-		},
+			endCol: 57	
+		},// 35 rows, 20 cols
 		top: {
 			startRow: 0,
 		  startCol: 17,
 			endRow: 9,
 			endCol: 40	
-		}
+		}, // 9 rows, 23 cols
+		bottom: {
+			startRow: 17,
+			startCol: 2,
+			endRow: 42,
+			endCol: 36
+		}//25 rows, 34 cols
 	}
 	const minW = 5; const maxW = 10; const minH = 5; const maxH = 10;
 	const params = [
 		spaceAround, room, { minW, maxW, minH, maxH }
 	]
 	const answer = getChildren(...params)
-	console.log(answer.right)
+	console.log(answer)
+  const { left, right, top, bottom } = answer;
 
-	//left coords tests
-		const { left } = answer
-		// horizontal tests
-		it('expect endCol to be room.startCol-1', ()=> {
-			expect(left.endCol).toEqual(room.startCol -1);	
+	//TODO: need to make a test case where the maxW and maxH can lead to a point outside the space available
+	// 			-- it is probably better to make a forEach params with the tests
+	//TODO: I also had the idea that could be better to test min and max values instead of actual values, but some values like actual width and height would still need to be tested as they are and on a later refactor it could lead to problems like the actual value no being in range of maxs and mins if I don't writte more tests before to make sure of this
+	
+	// check if min and max height and width are being respected
+		//left
+		left.width = left.endCol - left.startCol
+		left.height = left.endRow - left.startRow
+		it('check width and height of left', ()=> {
+			expect(left.width).toBeGreaterThanOrEqual(minW);
+			expect(left.height).toBeGreaterThanOrEqual(minH);
+			expect(left.width).toBeLessThanOrEqual(maxW);
+			expect(left.height).toBeLessThanOrEqual(maxH);
+		});	
+		//right
+		right.width = right.endCol - right.startCol
+		right.height = right.endRow - right.startRow
+		it('check width and height of right', ()=> {
+			expect(right.width).toBeGreaterThanOrEqual(minW);
+			expect(right.height).toBeGreaterThanOrEqual(minH);
+			expect(right.width).toBeLessThanOrEqual(maxW);
+			expect(right.height).toBeLessThanOrEqual(maxH);
 		});
-		it('expect that width is not less or more than minW and maxW', ()=> {
-			let width = left.endCol - left.startCol;
-			expect(width).toBeLessThanOrEqual(maxW);
-			expect(width).toBeGreaterThanOrEqual(minW);
+		//top
+		top.width = top.endCol - top.startCol
+		top.height = top.endRow - top.startRow
+		it('check width and height of top', ()=> {
+			expect(top.width).toBeGreaterThanOrEqual(minW);
+			expect(top.height).toBeGreaterThanOrEqual(minH);
+			expect(top.width).toBeLessThanOrEqual(maxW);
+			expect(top.height).toBeLessThanOrEqual(maxH);
 		});
-		// vertical tests
-		it('expect left.startRow less than room.endRow', ()=> {
-			expect(left.startRow).toBeLessThan(room.endRow);
+		//bottom
+		bottom.width = bottom.endCol - bottom.startCol
+		bottom.height = bottom.endRow - bottom.startRow
+		it('check width and height of bottom', ()=> {
+			expect(bottom.width).toBeGreaterThanOrEqual(minW);
+			expect(bottom.height).toBeGreaterThanOrEqual(minH);
+			expect(bottom.width).toBeLessThanOrEqual(maxW);
+			expect(bottom.height).toBeLessThanOrEqual(maxH);
+		});	
+
+	// check if non fixed cols and rows are inside of spaceAround
+		it('checks for left side to see if everything is inside space really available', ()=> {
+			expect(left.startRow).toBeGreaterThanOrEqual(spaceAround.left.startRow);
+			expect(left.startCol).toBeGreaterThanOrEqual(spaceAround.left.startCol);
+			expect(left.endRow).toBeLessThanOrEqual(spaceAround.left.endRow);
+			expect(left.endCol).toBeLessThanOrEqual(spaceAround.left.endCol);
 		});
-		it('expect left.startRow >= spaceAround.left.startRow', ()=> {
-			expect(left.startRow).toBeGreaterThanOrEqual(spaceAround.left.startRow);		
+		it('checks for right side to see if everything is inside space really available', ()=> {
+			expect(right.startRow).toBeGreaterThanOrEqual(spaceAround.right.startRow);
+			expect(right.startCol).toBeGreaterThanOrEqual(spaceAround.right.startCol);
+			expect(right.endRow).toBeLessThanOrEqual(spaceAround.right.endRow);
+			expect(right.endCol).toBeLessThanOrEqual(spaceAround.right.endCol);
 		});
-		it('expects left.endRow <= spaceAround.left.endRow', ()=> {
-			expect(left.endRow).toBeLessThanOrEqual(spaceAround.left.endRow)
+		it('checks for top side to see if everything is inside space really available', ()=> {
+			expect(top.startRow).toBeGreaterThanOrEqual(spaceAround.top.startRow);
+			expect(top.startCol).toBeGreaterThanOrEqual(spaceAround.top.startCol);
+			expect(top.endRow).toBeLessThanOrEqual(spaceAround.top.endRow);
+			expect(top.endCol).toBeLessThanOrEqual(spaceAround.top.endCol);
 		});
-		it('expect left.endRow > room.startRow', ()=> {
-			expect(left.endRow).toBeGreaterThan(room.startRow)
-		});
-		it('expects height >= minH, height <= maxW', ()=> {
-			let height = left.endRow - left.startRow;
-			expect(height).toBeGreaterThanOrEqual(minH);
-			expect(height).toBeLessThanOrEqual(maxH);
+		it('checks for bottom side to see if everything is inside space really available', ()=> {
+			expect(bottom.startRow).toBeGreaterThanOrEqual(spaceAround.bottom.startRow);
+			expect(bottom.startCol).toBeGreaterThanOrEqual(spaceAround.bottom.startCol);
+			expect(bottom.endRow).toBeLessThanOrEqual(spaceAround.bottom.endRow);
+			expect(bottom.endCol).toBeLessThanOrEqual(spaceAround.bottom.endCol);
 		});
 
-	//right coords tests
-		const { right } = answer;
-		// horizontal tests
-		it('expect startCol to be room.endCol+1', ()=> {
-			expect(right.startCol).toEqual(room.endCol +1);	
-		});
-		it('expect that width is not less or more than minW and maxW', ()=> {
-			let width = right.endCol - right.startCol;
-			expect(width).toBeLessThanOrEqual(maxW);
-			expect(width).toBeGreaterThanOrEqual(minW);
-		});
-		// vertical tests
-		it('expect left.startRow less than room.endRow', ()=> {
-			expect(right.startRow).toBeLessThan(room.endRow);
-		});
-		it('expect left.startRow >= spaceAround.left.startRow', ()=> {
-			expect(right.startRow).toBeGreaterThanOrEqual(spaceAround.right.startRow);		
-		});
-		it('expects left.endRow <= spaceAround.left.endRow', ()=> {
-			expect(right.endRow).toBeLessThanOrEqual(spaceAround.right.endRow)
-		});
-		it('expect left.endRow > room.startRow', ()=> {
-			expect(right.endRow).toBeGreaterThan(room.startRow)
-		});
-		it('expects height >= minH, height <= maxW', ()=> {
-			let height = right.endRow - right.startRow;
-			expect(height).toBeGreaterThanOrEqual(minH);
-			expect(height).toBeLessThanOrEqual(maxH);
-		});
+	// check if left and right startRows are less than room.endRow
+	it('check if left and right startRows are less than room.endRow', ()=> {
+		expect(left.startRow).toBeLessThan(room.endRow);
+		expect(right.startRow).toBeLessThan(room.endRow);
+	});
+	// check if left and right endRows are greater than room.startRow
+	it('check if left and right endRows are greater than room.StartRow', ()=> {
+		expect(left.endRow).toBeGreaterThan(room.startRow);
+		expect(right.endRow).toBeGreaterThan(room.startRow);
+	});
+	// check if top and bottom startCol are less than room.endCol
+	it('check if top and bottom startCol are less than room.endCol', ()=> {
+		expect(top.startCol).toBeLessThan(room.endCol);	
+		expect(bottom.startCol).toBeLessThan(room.endCol);	
+	});
+	// check if top and bottom endCol are greater than room.startCol
+	it('check if top and bottom endCol are greater than room.startCol', ()=> {
+		expect(top.endCol).toBeGreaterThan(room.startCol);
+		expect(bottom.endCol).toBeGreaterThan(room.startCol);
+	});
 
+	// check for fixed cols and rows
+	  it('checks if left endCol === spaceAround.left.endCol-1', ()=> {
+		  expect(left.endCol).toEqual(spaceAround.left.endCol-1);	
+	  });
+	  it('check if right startCol === spaceAround.right.startCol+1', ()=> {
+		  expect(right.startCol).toEqual(spaceAround.right.startCol+1);	
+	  });
+	  it('check if top endRow === spaceAround.top.endRow-1', ()=> {
+		  expect(top.endRow).toEqual(spaceAround.top.endRow-1);
+	  });
+	  it('check if the bottom startRow === spaceAround.bottom.endRow+1', ()=> {
+		  expect(bottom.startRow).toEqual(spaceAround.bottom.startRow+1)	
+	  });
 });
